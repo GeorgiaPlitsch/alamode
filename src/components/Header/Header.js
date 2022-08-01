fd13217fb2b1d0f8ae11bfec774d72e7c986b642
Georgia Plitsch <georgia.plitsch@outlook.com>
2022-08-01 17:02:47 +0100

    Started work

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__title">
+        <h1 className="header__title-text">ÀLaMode</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
