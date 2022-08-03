b0a3d62a588379b005044ca78a95adef99d03c6e
Georgia Plitsch <georgia.plitsch@outlook.com>
2022-08-03 15:20:20 +0100

    browse articles section started

---
+++ src/components/BrowseArticles/BrowseArticles.js
@@ -0,0 +1,11 @@
+import "./BrowseArticles.scss";
+
+const BrowseArticles = () => {
+  return (
+    <section>
+      <h3 className="browse__header">Browse articles...</h3>
+    </section>
+  );
+};
+
+export default BrowseArticles;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,23 +1,12 @@
 import "./Home.scss";
-import downArrow from "../../assets/icons/down-arrow.png";
-
+import HomeHero from "../../components/HomeHero/HomeHero";
+import BrowseArticles from "../../components/BrowseArticles/BrowseArticles";
 const Home = () => {
   return (
-    <section className="home">
-      <div className="home__hero">
-        <h2 className="home__hero-header">
-          Start making responsible <br></br> fashion choices <br></br>today.
-        </h2>
-
-        <button className="home__hero-button">
-          <h4>THE SUSTAINIBILTY QUIZ</h4>
-        </button>
-
-        <div>
-          <img className="home__hero-down" src={downArrow}></img>
-        </div>
-      </div>
-    </section>
+    <>
+      <HomeHero />
+      <BrowseArticles />
+    </>
   );
 };
 
