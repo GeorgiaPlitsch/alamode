5476c7a57365f9de40600af4edd6b801d84403a7
Georgia Plitsch <georgia.plitsch@outlook.com>
2022-08-03 12:26:13 +0100

    added api calls to api file

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -2,13 +2,23 @@ import axios from "axios";
 
 const apiUrl = "http://localhost:8080/api";
 
-export const getFabricInfo = async (name) => {
-  if (!name) {
-    return;
-  }
+// export const getFabricInfo = async (name) => {
+//   if (!name) {
+//     return;
+//   }
 
+//   try {
+//     const res = await axios.get(`${apiUrl}/fabrics/${name}`);
+
+//     return res.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+export const getFabrics = async () => {
   try {
-    const res = await axios.get(`${apiUrl}/fabrics/${name}`);
+    const res = await axios.get(`${apiUrl}/fabrics`);
 
     return res.data;
   } catch (error) {
@@ -16,9 +26,19 @@ export const getFabricInfo = async (name) => {
   }
 };
 
-export const getFabrics = async () => {
+export const getArticles = async () => {
   try {
-    const res = await axios.get(`${apiUrl}/fabrics`);
+    const res = await axios.get(`${apiUrl}/articles`);
+
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getResults = async () => {
+  try {
+    const res = await axios.get(`${apiUrl}/results`);
 
     return res.data;
   } catch (error) {
