21e0e66f559466da9f09dd90c6585cebebea89a6
Georgia Plitsch <georgia.plitsch@outlook.com>
2022-08-03 14:51:53 +0100

    hero created and styled, logo styling updated

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,22 @@
 import "./Home.scss";
+import downArrow from "../../assets/icons/down-arrow.png";
 
 const Home = () => {
   return (
     <section className="home">
-      <div className="home__hero"></div>
+      <div className="home__hero">
+        <h2 className="home__hero-header">
+          Start making responsible <br></br> fashion choices <br></br>today.
+        </h2>
+
+        <button className="home__hero-button">
+          <h4>THE SUSTAINIBILTY QUIZ</h4>
+        </button>
+
+        <div>
+          <img className="home__hero-down" src={downArrow}></img>
+        </div>
+      </div>
     </section>
   );
 };
