ed300d3a8d965022cbeb240660288bcf1033fbcd
Georgia Plitsch <georgia.plitsch@outlook.com>
2022-08-04 18:16:03 +0100

    app routed, menu links routed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { BrowserRouter, Redirect, Route, Switch, Link } from "react-router-dom";
+import React from "react";
+
 import "./App.scss";
 import { slide as Menu } from "react-burger-menu";
 import Header from "./components/Header/Header";
@@ -9,32 +12,39 @@ import FabricChecker from "./pages/FabricChecker/FabricChecker";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Menu pageWrapId={"page-wrap"} />
-      <main id="page-wrap">
-        <Menu className="App__menu">
-          <h1>ÀLaMode</h1>
-          <a className="menu-item" href="/">
-            <h3>Home</h3>
-          </a>
-          <a className="menu-item" href="/about">
-            <h3>Fabric Composition Checker</h3>
-          </a>
-          <a className="menu-item" href="/contact">
-            <h3>Sustainability Quiz</h3>
-          </a>
-          <a className="menu-item" href="">
-            <h3>Articles</h3>
-          </a>
-          <a className="menu-item" href="">
-            <h3>About</h3>
-          </a>
-        </Menu>
-        {/* <Home /> */}
-        {/* <Articles /> */}
-        <FabricChecker />
-        <Footer />
-      </main>
+      <BrowserRouter>
+        <Header />
+        <Menu pageWrapId={"page-wrap"} />
+        <main id="page-wrap">
+          <Menu className="App__menu">
+            <h1>ÀLaMode</h1>
+            <Link to="/" className="menu-item">
+              <h3>Home</h3>
+            </Link>
+            <Link to="/fabricchecker" className="menu-item">
+              <h3>Fabric Composition Checker</h3>
+            </Link>
+            <Link to="/quiz" className="menu-item">
+              <h3>Sustainability Quiz</h3>
+            </Link>
+            <Link to="/articles" className="menu-item" href="">
+              <h3>Articles</h3>
+            </Link>
+            <Link to="/about" className="menu-item">
+              <h3>About</h3>
+            </Link>
+          </Menu>
+
+          <Switch>
+            <Redirect from="/home" to="/" />
+            <Route exact path="/" component={Home} />
+            <Route path="/articles" component={Articles} />
+            <Route path="/fabricchecker" component={FabricChecker} />
+          </Switch>
+
+          <Footer />
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
