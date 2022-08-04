15dbfbd7eeebb9dedec8003378d107e8b36b1807
Georgia Plitsch <georgia.plitsch@outlook.com>
2022-08-04 12:32:30 +0100

    staged for next feature

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ function App() {
             <h3>About</h3>
           </a>
         </Menu>
-        {/* <Home /> */}
-        <Articles />
+        <Home />
+        {/* <Articles /> */}
         {/* <FabricChecker /> */}
         <Footer />
       </main>
