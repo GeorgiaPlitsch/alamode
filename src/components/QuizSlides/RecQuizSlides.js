87d787a3f28e82c370ce8703271f54ff8a6495f7
Georgia Plitsch <georgia.plitsch@outlook.com>
2022-08-07 10:45:20 +0100

    functionality to render reccomendation question implemented, not rendering results

--- src/components/QuizSlides/RecQuizSlides.js
+++ src/components/QuizSlides/RecQuizSlides.js
@@ -2,46 +2,51 @@ import "./QuizSlides.scss";
 import React, { useState } from "react";
 
 const RecQuizSlides = (props) => {
-  const [Question, setQuestion] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
   const [Result, setResult] = useState(0);
 
   const questions = props.questions;
   const score = props.score;
 
-  const handleAnswerClick = () => {
-    setResult(question.resultId);
+  const handleAnswerClick = (result) => {
+    if (currentQuestion === 0) {
+      findRecQuestion(result);
+    } else {
+      setResult(result);
+      return (
+        <>
+          <p>results</p>
+          <p>results</p>
+          <p>results</p>
+          <p>results</p>
+        </>
+      );
+    }
+  };
+
+  const findRecQuestion = (answerResult) => {
+    const filteredQuestion = questions.findIndex(
+      (question) => question.id === answerResult
+    );
+    setCurrentQuestion(filteredQuestion);
   };
 
   const answers = questions[currentQuestion].answers.map((answer) => (
-    <button onClick={() => handleAnswerClick(answer.answerScore)}>
+    <button onClick={() => handleAnswerClick(answer.resultId)}>
       {answer.answerText}
     </button>
   ));
 
   return (
-    //   <div className="quiz">
-    //     <div className="quiz__question">
-    //       <p className="quiz__question-count">Question {currentQuestion + 1}</p>
-    //       <div className="quiz__question-text">
-    //         {questions[currentQuestion].questionText}
-    //       </div>
-    //     </div>
-    //     <div className="quiz__answer-section">{answers}</div>
-    //   </div>
-    <>
-      <p>piu</p>
-      <p>piu</p>
-      <p>piu</p>
-      <p>piu</p>
-      <p>piu</p>
-      <p>piu</p>
-      <p>piu</p>
-      <p>piu</p>
-      <p>piu</p>
-      <p>piu</p>
-      <p>piu</p>
-      <p>piu</p>
-    </>
+    <div className="quiz">
+      <div className="quiz__question">
+        <p className="quiz__question-count">Question {currentQuestion + 1}</p>
+        <div className="quiz__question-text">
+          {questions[currentQuestion].questionText}
+        </div>
+      </div>
+      <div className="quiz__answer-section">{answers}</div>
+    </div>
   );
 };
 
