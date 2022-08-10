0884cb3d66787e7a1393e607a3ed3a10015f41a1
Georgia Plitsch <georgia.plitsch@outlook.com>
2022-08-10 17:10:57 +0100

    Head name change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <section className="footer">
-      <p className="footer__text">by Georgia Plitsch 2022</p>
+      <p className="footer__text"> Ⓒ ÀLaMode, Georgia Plitsch 2022</p>
     </section>
   );
 };
