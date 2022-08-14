d98987954ababcaa194ec2569568c86d32783690
Georgia Plitsch <georgia.plitsch@outlook.com>
2022-08-14 17:00:20 +0100

    fabric checker advice bullet styling

--- src/components/FabricCheckerAdvice/FabricCheckerAdvice.js
+++ src/components/FabricCheckerAdvice/FabricCheckerAdvice.js
@@ -7,7 +7,9 @@ const FabricCheckerAdvice = () => {
         <h3 className="advice__header">Other considerations...</h3>
         <h5 className="advice__about">
           Remember that although it's ideal to only buy the most sustainable
-          fabrics, it's not the only factor to consider. <br></br>
+          fabrics, it's not the only factor to consider.
+        </h5>
+        <h5 className="advice__about">
           Sometimes an item made from not so eco-friendly materials may still be
           the right option or a seemingly sustainable item may not be the best
           choice.
@@ -15,19 +17,19 @@ const FabricCheckerAdvice = () => {
       </div>
       <div className="advice__container-text">
         <p className="advice__text">
-          .Is the item well constructed and of high quality?
+          • Is the item well constructed and of high quality?
         </p>
       </div>
       <div className="advice__container-text">
         <p className="advice__text">
-          .Is there a more sustainable alternative that's still accessible to
+          • Is there a more sustainable alternative that's still accessible to
           you?
         </p>
       </div>
       <div className="advice__container-text">
         <p className="advice__text">
-          .Is it an item you see yourself wearing for years to come? Or is it an
-          occassion piece? If so, check whether it's available to rent via -
+          • Is it an item you see yourself wearing for years to come? Or is it
+          an occassion piece? If so, check whether it's available to rent via -
           <a className="advice__link" href="https://byrotation.com/">
             ByRotation
           </a>
@@ -35,7 +37,7 @@ const FabricCheckerAdvice = () => {
       </div>
       <div className="advice__container-text">
         <p className="advice__text">
-          .Have you checked whether the item is available to buy second-hand?
+          • Have you checked whether the item is available to buy second-hand?
           Check on -
           <a className="advice__link" href="https://www.depop.com/">
             Depop
